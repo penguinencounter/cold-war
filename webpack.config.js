96fd9b845bf7431c80e55a40362122ca6211eb3a
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-05-04 12:48:49 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = {
+    loader: 'file-loader',
+    options: {}
+};
+
+
+
+const config = {
+    entry: {
+        index: ['./src/index.ts', './src/index.scss'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[hash][ext][query]'
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'ts-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.css$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {loader: 'css-loader', options: {sourceMap: true, importLoaders: 2}},
+                    {loader: 'sass-loader', options: {sourceMap: true}}
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
